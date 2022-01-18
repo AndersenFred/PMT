aabc85db97039a51c7052894dba7df1afb012220
Frederik Andersen <82170487+AndersenFred@users.noreply.github.com>
2022-01-18 14:54:18 +0100

    Update Data_analysis.py

--- Data_analysis.py
+++ Data_analysis.py
@@ -9,12 +9,17 @@ e = scipy.constants.e
 from scipy.optimize import curve_fit as curve_fit
 
 def doppel_gauss(x,mu_1,sig_1,ampl_1,mu_2,sig_2,ampl_2):
+    '''A function with two normal distributions'''
     return ampl_1/(np.sqrt(2*np.pi)*sig_1)*np.exp(-(x-mu_1)**2/(2*sig_1**2))\
     +ampl_2/(np.sqrt(2*np.pi)*sig_2)*np.exp(-(x-mu_2)**2/(2*sig_2**2))
+
+
 def gauss(x,mu,sigma,a):
+    ''' A function with a normal distribution'''
     return a*np.exp(-(x-mu)**2/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma)
 
 def read_rawdata_from_file(filepath):
+    '''Read rawdata from file'''
     retval = dict()
     with h5py.File(filepath, 'r') as file:
         retval['samplerate'] = float(file.attrs[u'samplerate'])
@@ -26,6 +31,7 @@ def read_rawdata_from_file(filepath):
     return retval
 
 def save_rawdata_to_file(filepath, data, Measurement_time, y_off, YMULT, samplerate, HV):
+    '''save rawdata to file'''
     with h5py.File('{}.h5'.format(filepath),'w') as file:
         file.attrs[u'samplerate'] = samplerate
         file.attrs[u'measurement_time'] = Measurement_time
@@ -53,16 +59,20 @@ def values(retval = None, filename = None):
     return data, measurement_time, y_off, YMULT, samplerate, HV
 
 def x_y_values(data, Measurement_time, y_off, YMULT, samplerate = None):
+    '''Method to get the x and y values from rawdata'''
     x = np.linspace(0,Measurement_time,len(data[0,:]))
     y = ((data)*YMULT+y_off).T
     return x,y
 def numinteg(x,y):
+    '''simple Method for numeric integration'''
     return np.sum((x[1]-x[0])*y)
 
 def number_of_electrons(x,y,R=50):
+    '''simple Method for estimating the number of electrons passing the oszi'''
     return numinteg(x,y)*R/e
 
 def plot(x,y,figsize = (10,5), index = False):
+    '''plot for lazy people'''
     if (not index):
         x = np.linspace(1,len(y),len(y))
     fig, ax = plt.subplots(figsize = figsize)
@@ -74,16 +84,15 @@ def mean_plot(y):
     ax.plot(np.mean(y, axis = 0))
     plt.show()
 
-def hist(waveforms, ped_min=60, ped_max=300, sig_min=500, sig_max=1500, histo_range= None, plot = True, save = False, name = None):
+def hist(waveforms, ped_min=60, ped_max=180, sig_min=200, sig_max=400, histo_range= None, plot = True, save = False, name = None):
     ped_sig_ratio = (ped_max - ped_min) / (sig_max - sig_min)
     pedestals = np.sum(waveforms[:, ped_min:ped_max], axis=1)
     charges = (np.sum(waveforms[:, sig_min:sig_max], axis=1) - pedestals * ped_sig_ratio)
     if plot:
-        plt.hist(charges, range=None, bins=200, log=True)
+        fig, ax = plt.subplots(figsize= (10,5))
+        ax.hist(charges, range=None, bins=200, log=True)
         plt.show()
         if save:
-            fig, ax = plt.subplots(figsize= (10,5))
-            ax.hist(charges, range=None, bins=200, log=True)
             fig.savefig(name)
     return np.histogram(charges, range = histo_range, bins = 200)
 
