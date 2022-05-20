60aeb165e05de6c48bd782850e64f01acc64e356
Frederik Andersen <82170487+AndersenFred@users.noreply.github.com>
2022-05-20 14:33:24 +0200

    Update variating_int_range.py

--- variating_int_range.py
+++ variating_int_range.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import Data_analysis as data
+import time
 
 
 HV = '1170_0'
@@ -36,10 +37,16 @@ fig.savefig(f'variating_int_range/Variating signal min {SN}, HV = {HV}.pdf')
 
 
 ped_min, ped_max, sig_min, sig_max = 750, 999, 250, 350
-
+before = time.time()
 sig_min, sig_max, gains = data.hist_variable_values(waveforms, ped_min = ped_min, ped_max = ped_max,\
-sig_min_start = sig_min, sig_max_start = sig_max,h_int = h_int, interval_sig_min = 3,\
-interval_sig_max = 5, number_sig_min = 50, number_sig_max = 50)
+sig_min_start = sig_min, sig_max_start = sig_max,h_int = h_int, interval_sig_min = 1,\
+interval_sig_max = 1, number_sig_min = 20, number_sig_max = 20)
+measurment_time = time.time()-before
+hours = int(measurment_time/3600)
+minutes = int((measurment_time-hours*3600)/60)
+seconds = int((measurment_time-hours*3600-minutes*60))
+print(f'computing time: {hours}h{minutes}m{seconds}s')
+
 data.plot_hist_variable_values(sig_min, sig_max, gains, name = 'variating_int_range/Variating_signal_int_ranges {SN}, HV = {HV}.pdf')
 
 
