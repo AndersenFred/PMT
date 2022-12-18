1db3e3ed87db2fbc3c87755583901ec10c584467
Frederik Andersen <82170487+AndersenFred@users.noreply.github.com>
2022-12-18 23:10:55 +0100

    Update messung.py

--- messung.py
+++ messung.py
@@ -1,9 +1,10 @@
-import Inst_control as inst
+import modules.inst_control as inst
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import Data_analysis as data
+import modules.data_analysis as data
 import sys
+import os
 try:
     SN = sys.argv[1]
     HV = int(sys.argv[2])
@@ -20,24 +21,65 @@ except IndexError:
 except TypeError:
     number = 10000
 
-SHR = inst.SHR(volt = HV)
-SHR.voltage(HV)
-SHR.output_on()
-osci =  inst.Osci()
+func_gen = inst.Funk_Gen()
+func_gen.pulse(freq = 1e3, ampl = 1, off=0, width = 100e-9, channel = 1)
+func_gen.on()
+#SHR = inst.SHR(volt = HV)
+#SHR.voltage(HV)
+#SHR.output_on()
+osci = inst.Osci()
+time.sleep(1)
 osci.write('HOR:FAST:STATE OFF')
 osci.write('DISplay:WAVEform ON')
+osci.write('ACQ:STATE RUN')
+path = f'/media/pmttest/TOSHIBA EXT/Messdaten/KM3Net_{SN}'
+try:
+    os.mkdir(path)
+except FileExistsError:
+    pass
+path += f'/{HV}'
+
+try:
+    os.mkdir(path)
+except FileExistsError:
+    pass
+
 while (True):
     if 'y' == input(f'HV = {HV}\nCorrect HV and correct scale?(y)\n'):
         break
-h5_filename = 'Messdaten/KM3Net_{}.h5'.format(SN)
-y_values, Measurement_time, YOFF, YMU, samplerate = osci.messung(number, Measurement_time =8e-8,vertical_delay = 180e-9, samplerate = 12.5e9, Max_Ampl = 80e-3)
-i = data.save_rawdata_to_file(h5_filename,y_values, Measurement_time, YOFF, YMU, samplerate, HV)
-x,y = data.x_y_values(y_values, YOFF, YMU, Measurement_time, samplerate)
+
+h5_filename = f'/media/pmttest/TOSHIBA EXT/Messdaten/KM3Net_{SN}.h5'
+y_values, Measurement_time, YOFF, YMU, samplerate = osci.messung(number, Measurement_time = 8e-8, vertical_delay = 180e-9, samplerate = 12.5e9, Max_Ampl = 80e-3)
+try:
+    data.save_rawdata_to_file(h5_filename,y_values, Measurement_time, YOFF, YMU, samplerate, HV)
+except ValueError:
+    import h5py
+    with h5py.File(h5_filename, "r+") as f:
+        del f[f"{HV}"]
+        f.close()
+    data.save_rawdata_to_file(h5_filename, y_values, Measurement_time, YOFF, YMU, samplerate, HV)
+
+x, y = data.x_y_values(y_values, YOFF, YMU, Measurement_time, samplerate)
 h_int = 1/samplerate
 waveforms = y
-y,x, int_ranges = data.hist(waveforms, plot = True)
-gain, nphe, gain_err =  data.hist_fitter(y,x,h_int)
-data.add_fit_results(h5_filename, f'{HV}_{i}', gain, nphe,gain_err, int_ranges)
+plot = False
+if not HV % 100:
+    plot = True
+y, x, int_ranges = data.hist(waveforms, plot = plot, path = path)
+gain, nphe, gain_err = data.hist_fitter(y,x,h_int, plot = plot, path = path, title = f'SN: {SN}, HV: {HV}')
 print('Gain: ' ,gain, 'pm', gain_err)
+if (gain < 3e6 and gain + gain_err > 3e6) or (gain > 3e6 and gain - gain_err < 3e6):
+    print('Nominal Gain is within the error')
+elif gain >3e6:
+    print('Gains is too high')
+elif gain < 3e6:
+    print('Gain is too low')
+
 print('Number Photoelektrons: ', nphe)
+data.add_fit_results(h5_filename, f'{HV}', gain, nphe,gain_err, int_ranges)
+
+reader = data.WavesetReader(h5_filename)
+if number > 10000 or len(reader.wavesets) > 4:
+    data.analysis_complete_data(h5_filename, reanalyse= True, saveresults = True, SN = sys.argv[1], plot = True)
+    func_gen.off()
 #data.analysis_complete_data('Messdaten/KM3Net_AB2363.h5', 1167)
